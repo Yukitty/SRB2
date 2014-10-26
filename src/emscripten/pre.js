2581747bbbe3c4deb7ec72917bfe866151b00864
JTE <Jason.the.Echidna@gmail.com>
2014-10-26 04:38:51 -0400

    Revert "Moved filesystem stuff to post.js"
    This reverts commit c7aa9eb34705c890d5c3fd0127e509c1ae0cfad6.

--- src/emscripten/pre.js
+++ src/emscripten/pre.js
@@ -1,5 +1,5 @@
-Module["elementPointerLock"] = true
-Module["printErr"] = function(x) {
+Module.elementPointerLock = true
+Module.printErr = function(x) {
 	console.error(x)
 	if (x.substr(0,7) === "Error: ") // I_Error prepends this to its messages.
 		alert(x.substr(7))
