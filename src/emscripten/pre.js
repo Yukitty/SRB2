189fc4bef9f7962760948debe6f7799bcfc093d0
JTE <Jason.the.Echidna@gmail.com>
2014-10-26 05:09:11 -0400

    Baked pre.js into shell.html, so it is no longer necessary.

--- src/emscripten/pre.js
+++
@@ -1,6 +0,0 @@
-Module.elementPointerLock = true
-Module.printErr = function(x) {
-	console.error(x)
-	if (x.substr(0,7) === "Error: ") // I_Error prepends this to its messages.
-		alert(x.substr(7))
-}
